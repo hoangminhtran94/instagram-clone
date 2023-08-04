36fc69a069bd56875e1ded682718904bc55b9cff
minhhoangtran94 <95241885+hoangminhtran94@users.noreply.github.com>
2023-08-03 22:53:27 -0400

    fixing bugs, adding pwa, update icons

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  // register: true,
+  // scope: '/app',
+  // sw: 'service-worker.js',
+  //...
+});
 const nextConfig = {
   images: {
     domains: ["images.unsplash.com"],
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
