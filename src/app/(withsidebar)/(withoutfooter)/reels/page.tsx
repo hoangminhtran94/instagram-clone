4257962090ae029211fdef217a6d02b49e99f225
minhhoangtran94 <95241885+hoangminhtran94@users.noreply.github.com>
2023-08-29 02:24:03 -0400

    update video player

--- src/app/(withsidebar)/(withoutfooter)/reels/page.tsx
+++ src/app/(withsidebar)/(withoutfooter)/reels/page.tsx
@@ -1,7 +1,7 @@
 import VideoPlayer from "@/components/UI/VideoPlayer/VideoPlayer";
 const Reels = () => {
   return (
-    <div className="flex flex-col  items-center max-h-screen  snap-y snap-mandatory overflow-scroll pt-10 scroll-pt-10  hide-scroll-bar ">
+    <div className="flex flex-col gap-10  items-center max-h-screen  snap-y snap-mandatory overflow-scroll pt-5 scroll-pt-5  hide-scroll-bar ">
       <VideoPlayer />
       <VideoPlayer />
       <VideoPlayer />
---
+++ src/components/ReelsComponents/ReelVideoPlayer.tsx
@@ -0,0 +1,6 @@
+import VideoPlayer from "../UI/VideoPlayer/VideoPlayer";
+const ReelVideoPlayer = () => {
+  return <div></div>;
+};
+
+export default ReelVideoPlayer;
